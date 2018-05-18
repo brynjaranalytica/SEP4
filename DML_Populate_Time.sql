8401bf371b9c278c7fa278f1e96b64d71b5135e8
Deyana <deyana.atanasova04@gmail.com>
2018-05-18 11:05:32 +0200

    Populate D_Time

---
+++ DML_Populate_Time.sql
@@ -0,0 +1,22 @@
+-- Time dimension population:
+delete from D_Time;
+
+declare
+  selected_time date := to_date('00:00:00', 'HH24:MI:SS');
+--you can choose until when do you want to insert time
+  last_time date := to_date('23:59:59', 'HH24:MI:SS');
+--or how many rows do you want to insert
+  no_of_rows int := 100;
+begin
+  while selected_time <= last_time loop
+    insert into D_Time(hour, minute, second)
+    values(
+      to_number(to_char(selected_time, 'HH24')),
+      to_number(to_char(selected_time, 'MI')),
+      to_number(to_char(selected_time, 'SS'))
+    );
+
+    selected_time := selected_time + INTERVAL '1' SECOND;
+  end loop;
+  commit;
+end;
