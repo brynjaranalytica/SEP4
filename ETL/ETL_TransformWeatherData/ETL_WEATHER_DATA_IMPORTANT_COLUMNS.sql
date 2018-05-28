f2bbb9b9011f0a508fe93525856357774e4dd726
AnDron1c <alina.kaly@gmail.com>
2018-05-28 12:25:51 +0200

    create table ETL_WEATHER_DATA_CHANGED_DATATYPES

--- ETL/ETL_TransformWeatherData/ETL_WEATHER_DATA_IMPORTANT_COLUMNS.sql
+++ ETL/ETL_TransformWeatherData/ETL_WEATHER_DATA_IMPORTANT_COLUMNS.sql
@@ -41,6 +41,61 @@ insert into ETL_WEATHER_DATA_IMPORTANT_COLUMNS
         , sky_level_1_coverage
         , sky_level_1_altitude_feet
   from external_weather_data;
+ 
+ 
+---------------------------------------------------------------------------------------------------------------------
+ create table ETL_WEATHER_DATA_CHANGED_DATATYPES as (select *
+                                    from ETL_WEATHER_DATA_IMPORTANT_COLUMNS where 1 = 0); 
+  
+ALTER TABLE ETL_WEATHER_DATA_CHANGED_DATATYPES
+  MODIFY (
+    surface_temperature_fahrenheit number(6,0)
+  );
+  
+ALTER TABLE ETL_WEATHER_DATA_CHANGED_DATATYPES
+  MODIFY (
+    dew_point_temperature_fahrenheit number(6,0)
+  );
+  
+ ALTER TABLE ETL_WEATHER_DATA_CHANGED_DATATYPES
+  MODIFY (
+    dew_point_temperature_fahrenheit number(6,0)
+  );
+  
+alter table ETL_WEATHER_DATA_CHANGED_DATATYPES 
+rename column wind_direction_degrees to wind_direction;
+
+ALTER TABLE ETL_WEATHER_DATA_CHANGED_DATATYPES
+ MODIFY (
+    wind_direction char(2)
+  );
+
+ALTER TABLE ETL_WEATHER_DATA_CHANGED_DATATYPES
+ MODIFY (
+    wind_speed_knots number(6,0)
+  );
+  
+ALTER TABLE ETL_WEATHER_DATA_CHANGED_DATATYPES
+ MODIFY (
+    pressure_altimeter_inches number(6,0)
+  );
+
+ALTER TABLE ETL_WEATHER_DATA_CHANGED_DATATYPES
+ MODIFY (
+    visibility_miles number(6,0)
+  );
+  
+ALTER TABLE ETL_WEATHER_DATA_CHANGED_DATATYPES
+ MODIFY (
+    sky_level_1_coverage varchar2(4)
+  );
+  
+  ALTER TABLE ETL_WEATHER_DATA_CHANGED_DATATYPES
+ MODIFY (
+    sky_level_1_altitude_feet number(13)
+  );
+  
+  
   
   
 
