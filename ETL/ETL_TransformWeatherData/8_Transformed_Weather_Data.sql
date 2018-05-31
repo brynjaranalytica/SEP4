0596992b781e614867f195165406001c63c230a6
MadsOverballe <mads.overballe@gmail.com>
2018-05-31 21:30:20 +0200

    Surrogate key for weather data
    Needed a key for weather data rows and saw the comment saying that there was a potential issue with lack of PK, so I added a sequence to give each row a surrogate key.

--- ETL/ETL_TransformWeatherData/8_Transformed_Weather_Data.sql
+++ ETL/ETL_TransformWeatherData/8_Transformed_Weather_Data.sql
@@ -1,10 +1,23 @@
 drop table TRANSFORMED_WEATHER_DATA purge;
 
+drop sequence sq_transformed_weather_data_surrogate_key
+;
+  
+create sequence sq_transformed_weather_data_surrogate_key
+  increment by 1
+  start with 1
+  cache 100
+  nomaxvalue
+;
+
 -- Last step of transforming the weather data
 -- Put all of the transformed data in one table where
 -- all columns have the correct datatype and validated contents
-create table TRANSFORMED_WEATHER_DATA as (select *
+create table TRANSFORMED_WEATHER_DATA as (select 0 as surrogate_key, TRANSFORM_WEATHER_CLOUDS.*
                                   from TRANSFORM_WEATHER_CLOUDS where 1 = 0); 
+                                  
+create index ix_transformed_weather_data_date on transformed_weather_data(log_date);
+create index ix_transformed_weather_data_station on transformed_weather_data(station);
 
 -- good thing it's all fine
 -- bad thing is that we don't have a PK
@@ -14,7 +27,9 @@ create table TRANSFORMED_WEATHER_DATA as (select *
 -- ADD CONSTRAINT TransformWeatherDonePK PRIMARY KEY (station, log_date, log_hour, log_minute, log_second);
 
 insert into TRANSFORMED_WEATHER_DATA
-  select * from TRANSFORM_WEATHER_CLOUDS;
+  select sq_transformed_weather_data_surrogate_key.nextval as surrogate_key, TRANSFORM_WEATHER_CLOUDS.* from TRANSFORM_WEATHER_CLOUDS;
+
+execute dbms_stats.gather_table_stats('SEP4', 'TRANSFORMED_WEATHER_DATA');
 
 commit;
 /
