9aa98ac4df7c61ac87aa81f4189e0e2cf384fc43
MadsOverballe <mads.overballe@gmail.com>
2018-05-31 13:10:07 +0200

    Removed java implementation - you can always fetch it back from the history

--- ETL/ETL_MapGridCellToAirport/MapGridCellToAirport.sql
+++ ETL/ETL_MapGridCellToAirport/MapGridCellToAirport.sql
@@ -1,9 +1,22 @@
 /* This script locates the nearest weather station for each grid cell and 
  * assigns it to a new column in the dimension.
  */
+ 
+/* A view of every station and their coordinates is located in the external
+ * weather data.
+ */
+drop materialized view weather_stations
+;
 
-/* First the center of latitude and longitude is calculated for each grid cell
- * to be used as reference point of the cell
+create materialized view weather_stations as
+select distinct(station) as station_initials
+     , to_number(latitude, '99.9999') as latitude
+     , to_number(longitude, '99.9999') as longitude
+from external_weather_data
+;
+
+/* The center of latitude and longitude is calculated for each grid cell
+ * to be used as reference point of the cell.
  */
 alter table d_gridcell
 drop column latitude_center
@@ -24,23 +37,23 @@ add longitude_center as
 ;
 
 /* Then the distance to each weather station is calculated by the Pythagorean
- * equation for each grid cell
+ * equation for each grid cell.
  */
 drop materialized view distance_to_station
 ;
  
 create materialized view distance_to_station as
-select a.grid_cell_id, b.initials, sqrt(power(a.latitude_center - b.latitude, 2) + power(a.longitude_center - b.longitude, 2)) as distance
+select a.grid_cell_id, b.station_initials, sqrt(power(a.latitude_center - b.latitude, 2) + power(a.longitude_center - b.longitude, 2)) as distance
 from (select * from d_gridcell) a, weather_stations b
 ;
 
-/* Then the shortest distance for each grid cell is determined */
+/* Then the shortest distance for each grid cell is determined. */
 drop materialized view shortest_distance
 ;
 
 create materialized view shortest_distance as
-select grid_cell_id, initials as nearest_airport, distance
-from distance_to_airports a
+select grid_cell_id, station_initials as nearest_airport, distance
+from distance_to_station a
 where distance <= (select min(distance) from distance_to_station b where a.grid_cell_id = b.grid_cell_id)
 order by grid_cell_id
 ;
@@ -67,6 +80,9 @@ add nearest_weather_station char(4)
 
 update d_gridcell a
 set nearest_weather_station = (select nearest_airport from shortest_distance b where a.grid_cell_id = b.grid_cell_id)
+;
+
+drop materialized view weather_stations
 ;
 
 drop materialized view distance_to_station
