e307824e4343bc1906285895adb474485890e3d3
MadsOverballe <mads.overballe@gmail.com>
2018-06-06 19:15:51 +0200

    Analysis cleanup

--- ThermalAnalysis.sql
+++ ThermalAnalysis.sql
@@ -168,19 +168,3 @@ where strength_sample_size > 10 -- minimum sample size
 ;
 
 commit;
--- leftovers, contains threshold example
-/*
-create table thermal_analysis as
-select d_gridcell.latitude_center as latitude
-     , d_gridcell.longitude_center as longitude
-     , avg(f_movement.delta_altitude) as thermal_strength
-     , count(f_movement.grid_id) as sample_size
-from f_movement, d_gridcell
-where f_movement.grid_id = d_gridcell.grid_cell_id
-  and f_movement.delta_altitude > 0
-group by d_gridcell.latitude_center, d_gridcell.longitude_center
-/* threshold*//*
-having avg(f_movement.delta_altitude) > (1 + 10 / sqrt(count(f_movement.grid_id)))
-   and count(f_movement.grid_id) /*sample_size*//* > 10
-;
-*/
